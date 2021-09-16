c7faf2fce8f9f459b46c8aa1c50a6eb7e092a78f
OleziO <56752562+OleziO@users.noreply.github.com>
2021-09-17 00:59:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//-------------WIN FUNCTION-------------
+let showHorizontal = i => {
+    for (let j = 1; j <= 3; j++)
+        $(`.f${i}${j}`).css('background-color', 'lightgreen');
+    $('.restartBtn').css('animation', 'pulse 2s infinite');
+}
+
+let showVertical = i => {
+    for (let j = 1; j <= 3; j++)
+        $(`.f${j}${i}`).css('background-color', 'lightgreen');
+    $('.restartBtn').css('animation', 'pulse 2s infinite');
+}
+
+let showSlash = () => {
+    for (let j = 1; j <= 3; j++)
+        $(`.f${j}${j}`).css('background-color', 'lightgreen');
+    $('.restartBtn').css('animation', 'pulse 2s infinite');
+}
+
+let showBSlash = () => {
+    for (let j = 1; j <= 3; j++)
+        $(`.f${j}${4-j}`).css('background-color', 'lightgreen');
+    $('.restartBtn').css('animation', 'pulse 2s infinite');
+}
+//-------------GAME LOGIC-------------
+$(function () {
+    let char = 'X';
+    $('.field-wrapper').click(event => {
+        if ($(event.target).text() == '') {
+            $(event.target).text(char);
+            for (let i = 1; i <= 3; i++)
+                $(`.f${i}1`).text() == $(`.f${i}2`).text() &&
+                $(`.f${i}1`).text() == $(`.f${i}3`).text() &&
+                $(`.f${i}1`).text() != '' ?
+                showHorizontal(i) :
+                '';
+            for (let i = 1; i <= 3; i++)
+                $(`.f1${i}`).text() == $(`.f2${i}`).text() &&
+                $(`.f2${i}`).text() == $(`.f3${i}`).text() &&
+                $(`.f1${i}`).text() != '' ?
+                showVertical(i) :
+                '';
+            $(`.f11`).text() == $(`.f22`).text() &&
+                $(`.f11`).text() == $(`.f33`).text() &&
+                $(`.f11`).text() != '' ?
+                showSlash() :
+                '';
+
+            $(`.f13`).text() == $(`.f22`).text() &&
+                $(`.f13`).text() == $(`.f31`).text() &&
+                $(`.f13`).text() != '' ?
+                showBSlash() :
+                '';
+
+            char = char == 'X' ? 'O' : 'X';
+        } else
+            $(event.target).text();
+    });
+})
+//-------------PAGE ROLOAD-------------
+$('.restartBtn').click(() => location.reload());
+//-------------------------------------
